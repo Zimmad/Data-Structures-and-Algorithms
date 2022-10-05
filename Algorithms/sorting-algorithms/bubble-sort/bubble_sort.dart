52b72674add4dc78b07267b27a665d672695b580
Zimmad <muhammadzimmad224@gmail.com>
2022-10-05 16:45:17 +0500

    Bubble sort implemented over list

---
+++ Algorithms/sorting-algorithms/bubble-sort/bubble_sort.dart
@@ -0,0 +1,19 @@
+void bubbleSort<E extends Comparable<dynamic>>(List<E> list) {
+  void swap(int indexA, int indexB) {
+    final temp = list[indexA];
+    list[indexA] = list[indexB];
+    list[indexB] = temp;
+  }
+
+  var swaped = false;
+
+  for (var listLength = list.length - 1; listLength > 0; listLength) {
+    for (var current = 0; current < listLength; current++) {
+      if (list[current].compareTo(list[current + 1]) > 0) {
+        swap(current, current + 1);
+        swaped = true;
+      }
+    }
+    if (!swaped) return;
+  }
+}
